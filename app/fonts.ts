ef3a6a6cb7ee7878e289f6863b0e15e9023fc6f3
Arlan <arlankill12@gmail.com>
2024-11-28 16:46:15 -0800

    font fix

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,21 +1,21 @@
 import localFont from 'next/font/local'
 
 export const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: './fonts/GeistVF.woff2',
   variable: '--font-geist-sans',
   display: 'swap',
   weight: '100 900',
 })
 
 export const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: './fonts/GeistMonoVF.woff2',
   variable: '--font-geist-mono',
   display: 'swap',
   weight: '100 900',
 })
 
 export const consolas = localFont({
-  src: './fonts/CONSOLA.ttf',
+  src: './fonts/CONSOLA.woff2',
   variable: '--font-consolas',
   display: 'swap',
 })
@@ -23,37 +23,37 @@ export const consolas = localFont({
 export const overusedGrotesk = localFont({
   src: [
     {
-      path: './fonts/OverusedGrotesk-Light.ttf',
+      path: './fonts/OverusedGrotesk-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: './fonts/OverusedGrotesk-Book.ttf',
+      path: './fonts/OverusedGrotesk-Book.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './fonts/OverusedGrotesk-Medium.ttf',
+      path: './fonts/OverusedGrotesk-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './fonts/OverusedGrotesk-Roman.ttf',
+      path: './fonts/OverusedGrotesk-Roman.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: './fonts/OverusedGrotesk-SemiBold.ttf',
+      path: './fonts/OverusedGrotesk-SemiBold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: './fonts/OverusedGrotesk-Bold.ttf',
+      path: './fonts/OverusedGrotesk-Bold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: './fonts/OverusedGrotesk-Black.ttf',
+      path: './fonts/OverusedGrotesk-Black.woff2',
       weight: '900',
       style: 'normal',
     },
